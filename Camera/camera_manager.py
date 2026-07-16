5fb7454667b04a66a5be3e9f044c5bd09c6c249d
irih01 <199847844+irih01@users.noreply.github.com>
2026-07-16 23:04:44 +0300

    Update camera_manager.py

--- Camera/camera_manager.py
+++ Camera/camera_manager.py
@@ -3,8 +3,6 @@ from .flir_camera import FLIRCamera
 from .web_camera import WebCam
 from Errors import CameraError, safe_call, logger
 
-
-
 class CameraManager:
     """
     Manager unificat pentru camere.
@@ -23,7 +21,6 @@ class CameraManager:
         self.camera = None
         self.processor = None # Optonal: GPU/CPU
 
-    
     def create_camera(self, cam_type="webcam", **kwargs):
         """
         params:
@@ -33,7 +30,8 @@ class CameraManager:
         """
         cam_type = cam_type.lower().strip()
 
-        if self.camera:
+        if self.stream or self.camera:
+            logger.warning("[CameraManager] Se încearcă recreerea camerei. Se forțează oprirea fluxului vechi.")
             self.stop()
 
         if cam_type == "webcam":
@@ -43,13 +41,11 @@ class CameraManager:
         else:
             raise CameraError(f"Tip camera necunoscuta: {cam_type}")
         return self.camera
-    
 
-    
     def create_stream(self, camera, processor=None, fps=60):
         if not self.camera:
             raise CameraError("Nu exista camera creata. Apeleaza create")
-        
+
         self.processor = processor
         self.stream = CameraStream(camera, poll_interval=1.0/fps)
 
@@ -58,35 +54,39 @@ class CameraManager:
 
         logger.info("CameraMnager Stream creat.")
         return self.stream
-    
-
 
     def start(self):
         if not self.stream:
             raise RuntimeError("Streamul nu a fost creat")
         self.stream.start()
         logger.info("CamManager pornit")
-    
-    
+
     def stop(self):
+        error_messages = []
+
         if self.stream:
             try:
                 self.stream.stop()
             except Exception as e:
-                raise CameraError(f"[CameraManager] Eroare la oprire stream: {e}")
-            self.stream = None
+                error_messages.append(f"Eroare la oprire stream: {e}")
+            finally:
+                self.stream = None
 
         if self.camera:
             try:
                 self.camera.stop()
             except Exception as e:
-                raise CameraError(f"[CameraManager] Eroare la oprire camera: {e}")
-            self.camera = None
+                error_messages.append(f"Eroare la oprire camera: {e}")
+            finally:
+                self.camera = None
 
+        if error_messages:
+            combined_error = " | ".join(error_messages)
+            logger.error(f"[CameraManager] Probleme la închiderea resurselor: {combined_error}")
+            raise CameraError(f"[CameraManager] Defecțiune la curățarea resurselor: {combined_error}")
 
     def get_frame(self):
         return None if not self.stream else self.stream.stream_get_frame()
-    
+
     def is_running(self):
         return self.stream is not None
-
