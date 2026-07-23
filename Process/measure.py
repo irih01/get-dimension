7d35a5027c4eff172ca08ef4b9ee56e1d24a8d00
irih01 <199847844+irih01@users.noreply.github.com>
2026-07-23 14:08:44 +0300

    Update measure.py

--- Process/measure.py
+++ Process/measure.py
@@ -41,7 +41,8 @@ class MeasureMath:
     
     def area(self, contour):
         px2 = float(cv.contourArea(contour))
-        mm2 = self.px_to_mm(1)**2 * px2 if self.mm_per_px else None
+        # OPTIMIZARE: Calculăm direct pe baza pătratului raportului mm_per_px salvat
+        mm2 = (self.mm_per_px ** 2) * px2 if self.mm_per_px else None
         return {"px2": px2, "mm2": mm2}
     
     def calculate_a4(self, marker_corners, known_marker_size, a4_size = (21.0, 29.7)):
@@ -88,25 +89,29 @@ class MeasureView:
 
         cv.line(frame, p1_off, p2_off, color, thickness, cv.LINE_AA)
 
-        # Sageti
-        arrw_len = 10
-        cv.arrowedLine(frame, p1_off, p2_off, color, thickness, tipLength=0.03)
-        cv.arrowedLine(frame, p2_off, p1_off, color, thickness, tipLength=0.03)
+        # FIX TEXT: Verificăm tipul valorii pentru a asigura formatarea corectă
+        if isinstance(val, (int, float, np.floating)):
+            text = f'{val:.1f} {unit}'
+        else:
+            text = f'{val}'
+
+        # Sageti (FIX: Ajustat tipLength dinamic pe baza lungimii liniei pentru vizibilitate crescută)
+        tip_len = max(0.05, min(0.3, 15.0 / length))
+        cv.arrowedLine(frame, p1_off, p2_off, color, thickness, tipLength=tip_len)
+        cv.arrowedLine(frame, p2_off, p1_off, color, thickness, tipLength=tip_len)
 
         # Text
         mid = ((p1_off[0] + p2_off[0]) // 2, (p1_off[1] + p2_off[1]) // 2)
-        #text = f'{val:.1f} {unit}'
         
-        (tw, th), _ = cv.getTextSize(f'{val:.1f} {unit}', cv.FONT_HERSHEY_SIMPLEX, font_size, 1)
+        (tw, th), _ = cv.getTextSize(text, cv.FONT_HERSHEY_SIMPLEX, font_size, 1)
         txt_pos = (int(mid[0] - tw // 2), int(mid[1] - offset_vec[1] // 2))
 
         # Fundal alb
         cv.rectangle(frame, (txt_pos[0] - 4, txt_pos[1] - th - 4), 
                     (txt_pos[0] + tw + 4, txt_pos[1] + 4), (255, 255, 255), -1)
 
-
         # Text propriu-zis
-        cv.putText(frame, f'{val:.1f} {unit}', txt_pos, cv.FONT_HERSHEY_SIMPLEX, font_size, color, 1, cv.LINE_AA)
+        cv.putText(frame, text, txt_pos, cv.FONT_HERSHEY_SIMPLEX, font_size, color, 1, cv.LINE_AA)
 
         return frame
     
@@ -123,7 +128,6 @@ class MeasureView:
 
         return frame
     
-    
     @staticmethod
     def draw_box(frame, x, y, w, h, info):
         cv.rectangle(frame, (x, y), (x+w, y+h), (255, 200, 0), 2)
@@ -136,78 +140,3 @@ class MeasureView:
         cv.putText(frame, txt_px + txt_mm, (x, y - 10), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 200, 0), 2)
 
         return frame
-    
-
-
-
-
-
-class Measure:
-    """
-    Modul pentru masuratori in imagine folosind un factor de scala mm per px.
-    Poate fi utilizat cu orice sistem de calibrare (ex: ArUco).
-    """
-
-    def __init__(self, mm_per_px: float = None):
-        """mm_per_px: milimetri per pixel
-        Daca e None, functiile vor intoarce valori doar in pixeli.
-        """
-        self.mm_per_px = mm_per_px
-
-    
-    @staticmethod
-    def draw_dim_line(frame, pt1, pt2, val, 
-                      color=(0, 255, 0), 
-                      thickness=2, 
-                      font_sz=0.7, 
-                      unit="mm",
-                      offset=20):
-        pt1 = tuple(map(int, pt1))
-        pt2 = tuple(map(int, pt2))
-
-        # Vector driectie si lungime
-        dx, dy = pt2[0] - pt1[0], pt2[1] - pt1[1]
-        length = math.hypot(dx, dy)
-        if length == 0:
-            return frame
-        
-        # Vecor perpendicular
-        perp = np.array([-dy, dx], dtype=float)
-        perp /= np.linalg.norm(perp)
-        offset_vec = perp * offset
-
-        # Pcte pentru linia de cota
-        p1_off = (int(pt1[0] + offset_vec[0]), int(pt1[1] + offset_vec[1]))
-        p2_off = (int(pt2[0] + offset_vec[0]), int(pt2[1] + offset_vec[1]))
-
-        cv.line(frame, p1_off, p2_off, color, thickness, cv.LINE_AA)
-
-        # Sageti
-        arrw_len = 10
-        cv.arrowedLine(frame, p1_off, p2_off, color, thickness, tipLength=0.03)
-        cv.arrowedLine(frame, p2_off, p1_off, color, thickness, tipLength=0.03)
-
-        # Text
-        mid = ((p1_off[0] + p2_off[0]) // 2, (p1_off[1] + p2_off[1]) // 2)
-
-        if isinstance(val, (int, float, np.floating)):
-            text = f'{val:.1f} {unit}'
-        else:
-            text = f'{val}'
-        
-        (tw, th), _ = cv.getTextSize(text, cv.FONT_HERSHEY_SIMPLEX, font_sz, 1)
-        txt_pos = (int(mid[0] - tw // 2), int(mid[1] - offset_vec[1] // 2))
-
-        # Fundal alb
-        cv.rectangle(frame, (txt_pos[0] - 4, txt_pos[1] - th - 4), 
-                    (txt_pos[0] + tw + 4, txt_pos[1] + 4), (255, 255, 255), -1)
-
-
-        # Text propriu-zis
-        cv.putText(frame, text, txt_pos, cv.FONT_HERSHEY_SIMPLEX, font_sz, color, 1, cv.LINE_AA)
-
-        return frame
-
-   
-
-    
